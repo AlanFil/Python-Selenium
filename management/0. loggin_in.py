916cbd891454cd631a7c09849d316d2e1d867b69
AlanFil <49269767+AlanFil@users.noreply.github.com>
2021-02-15 00:47:41 +0100

    Add files via upload

---
+++ management/0. loggin_in.py
@@ -0,0 +1,28 @@
+import pyautogui
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome('C:/Users/user/PycharmProjects/Adding-automating-products/Utilities/chromedriver.exe')
+
+
+def logging_in():
+    pyautogui.hotkey('Win', 'up')
+    driver.implicitly_wait(3)
+
+    driver.get('')  # Panel do zarządzania stroną
+
+    driver.find_element_by_name('login[username]').send_keys('login')
+    driver.find_element_by_name('login[password]').send_keys('password')
+    driver.find_element_by_name('login[password]').send_keys(Keys.ENTER)
+
+    try:
+        driver.find_element_by_link_text('zamknij').click()
+    except NoSuchElementException:
+        pass
+
+    driver.get(
+        driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[3]/ul/li[3]/ul/li[1]/a').get_attribute('href')
+    )
+
+    return
